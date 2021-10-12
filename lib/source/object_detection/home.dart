b8a72023ccab13fa83e0d9bc137f03a04cc64ecb
Sanket Mote <sanketmote01@gmail.com>
2021-10-12 22:07:54 +0530

    Adding Object detection using ssdmobile net

---
+++ lib/source/object_detection/home.dart
@@ -0,0 +1,119 @@
+import 'dart:math' as math;
+
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:tflite/tflite.dart';
+
+import 'bndbox.dart';
+import 'camera.dart';
+
+const String ssd = "SSD MobileNet";
+const String yolo = "Tiny YOLOv2";
+
+class HomePage extends StatefulWidget {
+  final List<CameraDescription> cameras;
+
+  HomePage(this.cameras);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<dynamic>? _recognitions;
+  int _imageHeight = 0;
+  int _imageWidth = 0;
+  String _model = "";
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  loadModel() async {
+    String? res = await Tflite.loadModel(
+      model: "assets/models/ssd_mobilenet.tflite",
+      labels: "assets/models/ssd_mobilenet.txt",
+      // numThreads: 1, // defaults to 1
+      // isAsset:
+      //     true, // defaults to true, set to false to load resources outside assets
+      // useGpuDelegate:
+      //     false // defaults to false, set to true to use GPU delegate
+    );
+
+    print(res);
+  }
+
+  onSelect(model) {
+    setState(() {
+      _model = model;
+    });
+    loadModel();
+  }
+
+  setRecognitions(recognitions, imageHeight, imageWidth) {
+    setState(() {
+      _recognitions = recognitions;
+      _imageHeight = imageHeight;
+      _imageWidth = imageWidth;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size screen = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {
+              setState(() {
+                _model = "";
+              });
+            }),
+      ),
+      backgroundColor: Colors.white,
+      body: _model == ""
+          ? Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  RaisedButton(
+                    color: Colors.teal,
+                    child: const Text(
+                      ssd,
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    onPressed: () => onSelect(ssd),
+                  ),
+                  RaisedButton(
+                    color: Colors.teal,
+                    child: const Text(
+                      yolo,
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    onPressed: () => onSelect(yolo),
+                  ),
+                ],
+              ),
+            )
+          : Stack(
+              children: [
+                Camera(
+                  widget.cameras,
+                  _model,
+                  setRecognitions,
+                ),
+                BndBox(
+                  _recognitions == null ? [] : _recognitions,
+                  math.max(_imageHeight, _imageWidth),
+                  math.min(_imageHeight, _imageWidth),
+                  screen.height,
+                  screen.width,
+                ),
+              ],
+            ),
+    );
+  }
+}
